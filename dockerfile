d64f4bd9768d6e53f43027e9a9f19a07397510cb
emguse <30695541+emguse@users.noreply.github.com>
2024-11-01 18:37:53 +0900

    1st commit

---
+++ dockerfile
@@ -0,0 +1,15 @@
+ARG REGISTRY=quay.io
+ARG OWNER=jupyter
+ARG BASE_IMAGE=${REGISTRY}/${OWNER}/scipy-notebook
+FROM ${BASE_IMAGE}
+
+RUN conda install dataprep Polars plotly python-dateutil
+RUN conda install optuna keras
+RUN conda install mecab-python3 undic-lite
+RUN conda install python-docx pypdf
+RUN conda install Alembic psycopg sqlmodel
+RUN conda install requests pyyaml pytest boto3 python-dotenv
+RUN conda install jupytext jupyterlab-lsp python-lsp-server
+RUN pip install mip japanize-matplotlib opencv-contrib-python-headless
+
+CMD ["jupyterhub-singleuser", "--allow-root"]
---
+++ jupyterhub_config.py
@@ -0,0 +1,19 @@
+c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
+c.Authenticator.admin_users = {'admin'}
+
+from jupyter_client.localinterfaces import public_ips
+ip = public_ips()[0]
+c.JupyterHub.hub_ip = ip
+
+#c.JupyterHub.ssl_key = '/etc/ssl/server.key'
+#c.JupyterHub.ssl_cert = '/etc/ssl/server.crt'
+
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+c.Spawner.default_url = '/lab'
+c.DockerSpawner.default_url = '/lab'
+
+notebook_dir = '/home/jovyan/work'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = {'jupyterhub-user-{username}': notebook_dir}
+c.DockerSpawner.container_image = 'hub/hub1'
